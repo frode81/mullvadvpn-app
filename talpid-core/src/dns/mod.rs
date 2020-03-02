4469a0f7bd96f3d015b80751a9fb20d1c14fcbcb
David Lönnhager <david.l@mullvad.net>
2020-03-02 17:16:45 +0100

    Revert "Make DnsMonitor observable"
    This reverts commit 63e0f39cb3c6932ce55c03839060104f6e47f69c.

--- talpid-core/src/dns/mod.rs
+++ talpid-core/src/dns/mod.rs
@@ -16,60 +16,19 @@ mod imp;
 #[path = "android.rs"]
 mod imp;
 
-pub use self::imp::Error as OsError;
-
-/// Errors that can happen in the Linux DNS monitor
-#[derive(err_derive::Error, Debug)]
-pub enum Error {
-    /// Notification error
-    #[error(display = "An observer failed")]
-    Notification,
-
-    /// Implementation-specific error
-    #[error(display = "Internal DNS monitor error")]
-    Internal(#[error(source)] OsError),
-}
-
-/// Used to notify observers of DNS server changes.
-pub enum DnsMonitorUpdate<'a> {
-    /// System DNS was set.
-    Set {
-        /// Name of the network interface.
-        interface: &'a str,
-        /// List of DNS servers.
-        servers: &'a [IpAddr],
-    },
-    /// System DNS was reset.
-    Reset,
-}
+pub use self::imp::Error;
 
 /// Sets and monitors system DNS settings. Makes sure the desired DNS servers are being used.
 pub struct DnsMonitor {
     inner: imp::DnsMonitor,
-    observers: Vec<Box< dyn Fn(&DnsMonitorUpdate<'_>) -> Result<(), Error> >>
 }
 
 impl DnsMonitor {
     /// Returns a new `DnsMonitor` that can set and monitor the system DNS.
     pub fn new(cache_dir: impl AsRef<Path>) -> Result<Self, Error> {
         Ok(DnsMonitor {
-            inner: imp::DnsMonitor::new(cache_dir).map_err(Error::Internal)?,
-            observers: Vec::new(),
+            inner: imp::DnsMonitor::new(cache_dir)?,
         })
-    }
-
-    /// Register for notifications of DNS server changes.
-    pub fn observe<F>(&mut self, callback: F)
-        where F: 'static + Fn(&DnsMonitorUpdate<'_>) -> Result<(), Error>
-    {
-        self.observers.push(Box::new(callback));
-    }
-
-    fn notify(&self, state: DnsMonitorUpdate<'_>) -> Result<(), Error> {
-        for callback in &self.observers {
-            callback(&state)?;
-        }
-        Ok(())
     }
 
     /// Set DNS to the given servers. And start monitoring the system for changes.
@@ -82,19 +41,13 @@ impl DnsMonitor {
                 .collect::<Vec<String>>()
                 .join(", ")
         );
-        self.inner.set(interface, servers).map_err(Error::Internal)?;
-
-        self.notify(DnsMonitorUpdate::Set {
-            interface,
-            servers
-        })
+        self.inner.set(interface, servers)
     }
 
     /// Reset system DNS settings to what it was before being set by this instance.
     pub fn reset(&mut self) -> Result<(), Error> {
         log::info!("Resetting DNS");
-        self.inner.reset().map_err(Error::Internal)?;
-        self.notify(DnsMonitorUpdate::Reset)
+        self.inner.reset()
     }
 }
 

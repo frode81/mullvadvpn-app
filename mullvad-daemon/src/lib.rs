12b4cbaade5ee75759c42d63a70db4ccdb95f48d
David Lönnhager <david.l@mullvad.net>
2020-03-02 17:11:05 +0100

    Encapsulate functions in split

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -92,6 +92,9 @@ pub enum Error {
     #[error(display = "Unable to load account history with wireguard key cache")]
     LoadAccountHistory(#[error(source)] account_history::Error),
 
+    #[error(display = "Unable to initialize split tunneling")]
+    InitSplitTunneling(#[error(source)] split::Error),
+
     #[error(display = "No wireguard private key available")]
     NoKeyAvailable,
 
@@ -429,6 +432,7 @@ pub struct Daemon<L: EventListener> {
     tunnel_state: TunnelState,
     target_state: TargetState,
     state: DaemonExecutionState,
+    exclude_pids: split::PidManager,
     rx: Wait<UnboundedReceiver<InternalDaemonEvent>>,
     tx: DaemonEventSender,
     reconnection_loop_tx: Option<mpsc::Sender<()>>,
@@ -537,6 +541,7 @@ where
             tunnel_state: TunnelState::Disconnected,
             target_state: TargetState::Unsecured,
             state: DaemonExecutionState::Running,
+            exclude_pids: split::PidManager::new().map_err(Error::InitSplitTunneling)?,
             rx: internal_event_rx.wait(),
             tx: internal_event_tx,
             reconnection_loop_tx: None,
@@ -1281,7 +1286,7 @@ where
     #[cfg(unix)]
     fn on_get_split_tunnel_processes(&mut self, tx: oneshot::Sender<Vec<i32>>) {
         log::debug!("FIXME: on_get_split_tunnel_processes");
-        match split::list_pids() {
+        match self.exclude_pids.list() {
             Ok(pids) => Self::oneshot_send(tx, pids, "get_split_tunnel_processes response"),
             Err(e) => error!("{}", e.display_chain_with_msg("Unable to obtain PIDs")),
         }
@@ -1291,7 +1296,7 @@ where
     fn on_add_split_tunnel_process(&mut self, tx: oneshot::Sender<()>, pid: i32) {
         // TODO
         log::debug!("FIXME: on_add_split_tunnel_process");
-        match split::add_pid(pid) {
+        match self.exclude_pids.add(pid) {
             Ok(()) => Self::oneshot_send(tx, (), "add_split_tunnel_process response"),
             Err(e) => error!("{}", e.display_chain_with_msg("Unable to add PID")),
         }
@@ -1301,7 +1306,7 @@ where
     fn on_remove_split_tunnel_process(&mut self, tx: oneshot::Sender<()>, pid: i32) {
         // TODO
         log::debug!("FIXME: on_remove_split_tunnel_process");
-        match split::remove_pid(pid) {
+        match self.exclude_pids.remove(pid) {
             Ok(()) => Self::oneshot_send(tx, (), "remove_split_tunnel_process response"),
             Err(e) => error!("{}", e.display_chain_with_msg("Unable to remove PID")),
         }
@@ -1309,7 +1314,7 @@ where
 
     #[cfg(unix)]
     fn on_clear_split_tunnel_processes(&mut self, tx: oneshot::Sender<()>) {
-        match split::clear_pids() {
+        match self.exclude_pids.clear() {
             Ok(()) => Self::oneshot_send(tx, (), "clear_split_tunnel_processes response"),
             Err(e) => error!("{}", e.display_chain_with_msg("Unable to clear PIDs")),
         }
